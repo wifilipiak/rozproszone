7c2dc3cc124f57c950853f874199b2589dda20ed
wifilipiak <61492916+wifilipiak@users.noreply.github.com>
2020-06-01 14:39:19 +0200

    Update TSP_GA.py
    Add comments

--- TSP_GA.py
+++ TSP_GA.py
@@ -15,6 +15,7 @@ with open("bays29.json", "r") as tsp_data:
 distance_map = tsp["DistanceMatrix"]
 IND_SIZE = tsp["TourSize"]
 
+#Create classes
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
 creator.create("Individual", array.array, typecode='i', fitness=creator.FitnessMin)
 
@@ -34,7 +35,7 @@ def evalTSP(individual):
         distance += distance_map[gene1][gene2]
     return distance,
 
-
+#Add methods to toolbox
 toolbox.register("mate", tools.cxPartialyMatched)
 toolbox.register("mutate", tools.mutShuffleIndexes, indpb=0.05)
 toolbox.register("select", tools.selTournament, tournsize=3)
@@ -42,24 +43,45 @@ toolbox.register("evaluate", evalTSP)
 
 
 def main():
+    #Init random numbers generator with given seed (used in population generator)
     random.seed(265)
 
+    #Create population - list of Individual objects
     pop = toolbox.population(n=500)
 
+    #Create HallOfFame object to keep the best Individual
     hof = tools.HallOfFame(1)
+
+    #Create statistics object and add functions to apply
     stats = tools.Statistics(lambda ind: ind.fitness.values)
     stats.register("avg", numpy.mean)
     stats.register("std", numpy.std)
     stats.register("min", numpy.min)
     stats.register("max", numpy.max)
 
-    algorithms.eaSimple(pop, toolbox, 0.7, 0.2, 100, stats=stats, halloffame=hof)
+    #Run evolutionary algorithm
+    """
+    :param population: A list of individuals.
+    :param toolbox: A :class:`~deap.base.Toolbox` that contains the evolution
+                    operators.
+    :param cxpb: The probability of mating two individuals.
+    :param mutpb: The probability of mutating an individual.
+    :param ngen: The number of generation.
+    :param stats: A :class:`~deap.tools.Statistics` object that is updated
+                  inplace, optional.
+    :param halloffame: A :class:`~deap.tools.HallOfFame` object that will
+                       contain the best individuals, optional.
+    :param verbose: Whether or not to log the statistics.
+    """
+    algorithms.eaSimple(pop, toolbox, 0.7, 0.2, 100, stats=stats, halloffame=hof, verbose = False)
 
     return pop, stats, hof
 
 
 if __name__ == "__main__":
     pop, stats, hof = main()
+    #Run registered functions on pop and return it as dictionary
     results = stats.compile(pop)
+    #Change route format from <0,28> to <1,29> to match with json file
     best_route = [x+1 for x in hof[0].tolist()]
     print(f" min = {results['min']} \n best = {best_route}")
